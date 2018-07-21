a6a920852cb888e93cc494d21c5104d2fd212260
Oleksandr <oleksandrbuchek@gmail.com>
2018-07-22 02:09:00 +0300

    chore

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { UserStoreService } from '../../services/user-store.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'profile',
@@ -9,16 +11,21 @@ import { UserStoreService } from '../../services/user-store.service';
 })
 export class ProfileComponent implements OnInit {
 
-    // public userInfo: string[] = [];
-    // public userCards: any[] = [];
-    // public userSubscriptions: any[] = [];
+    public userInfo: string[] = [];
+    public userCards: any[] = [];
+    public userSubscriptions: any[] = [];
 
-    constructor(private userService: UserService, private userStore: UserStoreService) { }
+    constructor(private userService: UserService, 
+                private authService: AuthService,
+                private userStore: UserStoreService, 
+                private router: Router) { }
 
     ngOnInit() {
         this.userService.getUser().subscribe(({payload : user}) => {
-            console.log(user);
             this.userStore.setCurrentUser(user);
+        }, err => {
+            this.authService.deleteAuthorizationToken();
+            this.router.navigate(['login']);
         })
     }
 
